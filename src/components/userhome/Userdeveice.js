d8e4457f3cf79828207d10526281d8d3422a7260
xiaoyongyuan <2207294253@qq.com>
2019-10-16 15:19:17 +0800

    修改样式、布局

--- src/components/userhome/Userdeveice.js
+++ src/components/userhome/Userdeveice.js
@@ -419,7 +419,7 @@ class Userdeveice extends React.Component{
                     </div>
                     <Row className="equ_row">
                         <Col span={3} offset={20} className="t_l">
-                            <Button type="primary" className=" lg" onClick={this.updata} disabled={this.state.subBtn?true:false}> 提交 </Button>
+                            <div className="equBtn"><Button type="primary" className=" lg" onClick={this.updata} disabled={this.state.subBtn?true:false}> 提交 </Button></div>
                         </Col>
                     </Row>
                 </div>
